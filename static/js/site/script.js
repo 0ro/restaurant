d244adfeb3a734e0146c045e7b9fb66afc173536
Denis1990 <den.bogdanov4@gmail.com>
2015-10-01 17:36:16 +0300

    добавил блок team

--- static/js/site/script.js
+++ static/js/site/script.js
@@ -5,4 +5,21 @@ $(document).ready(function() {
     paginationSpeed : 400,
     singleItem:true
   });
+
+
+  $(".angle-down").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор блока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+
+
 });
